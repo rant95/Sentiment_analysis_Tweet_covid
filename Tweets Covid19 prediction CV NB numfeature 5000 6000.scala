ad8a35aaf2f929d20453cbf399bb8fb0fd62bf8c
Lova Rant <34003905+rant95@users.noreply.github.com>
2021-12-19 21:04:36 +0100

    Update and rename Corona prediction naives bayes numfeature 5000 6000.html to Tweets Covid19 prediction CV NB numfeature 5000 6000.scala

---
+++ Tweets Covid19 prediction CV NB numfeature 5000 6000.scala
@@ -0,0 +1,606 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Project Tweets Covid19 prediction CV
+// MAGIC 
+// MAGIC ### Naives Bayes Cross validation
+// MAGIC #### numfeature 5000 6000
+
+// COMMAND ----------
+
+import com.mongodb.spark._
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.functions._
+import com.databricks.spark.corenlp.functions._
+import org.apache.spark._
+
+import scala.collection.mutable
+import org.apache.spark.mllib.clustering.LDA
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
+
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.types.DoubleType
+
+import org.apache.spark.sql.functions.{from_unixtime, unix_timestamp, window}
+import org.apache.spark.sql.types.{StructType, StructField, StringType}
+import org.apache.spark.sql.functions._
+import sqlContext.implicits._
+import org.apache.spark.ml.feature.StopWordsRemover
+
+import java.sql.Timestamp
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.functions.to_timestamp
+
+// COMMAND ----------
+
+//val tweetFe = spark.table("data_trainv0")//.withColumn("Feels",col("Feelings").cast(DoubleType))  
+//val tweetTe = spark.table("data_testv0")//.withColumn("Feels",col("Feelings").cast(DoubleType)) 
+
+val tweetFe = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("database","allnews").option("collection","data_trainv0").load()
+val tweetTe = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("database","allnews").option("collection","data_testv0").load()
+val tweetFeel=tweetFe.withColumnRenamed("ctext","OriginalTweet")
+val tweetTest =tweetTe.withColumnRenamed("ctext","OriginalTweet")
+tweetFeel.printSchema()
+tweetTest.printSchema()
+
+// COMMAND ----------
+
+tweetFeel.show(5) 
+
+// COMMAND ----------
+
+tweetTest.show(5) 
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1. Tweet to Lemmetization
+
+// COMMAND ----------
+
+
+val withLemmes = tweetFeel.withColumn("lemmas", lemma(col("OriginalTweet")))
+val withLemmesString = withLemmes.withColumn("lemmas", concat_ws(" ",col("lemmas"))) //pour passer de lemme  tokenization
+//withLemmesString.show(7)
+//val withLemmesDF = withLemmes.select("Feelings","lemmas")
+//val withLemmesRDD = withLemmesDF.rdd.map(f => (f.getAs[String](0),f.getAs[String](1)))
+
+//Test data
+
+val withLemmesTest = tweetTest.withColumn("lemmas", lemma(col("OriginalTweet")))
+val withLemmesStringTest = withLemmesTest.withColumn("lemmas", concat_ws(" ",col("lemmas"))) //pour passer de lemme  tokenization
+
+// COMMAND ----------
+
+/*display(withLemmes
+    .select(explode(col("lemmas")).as("lemmas"))
+    .groupBy("lemmas")
+    .count()
+    .orderBy(col("count").desc).limit(10))*/
+    
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.2 Delete stop word
+
+// COMMAND ----------
+
+import org.apache.spark.ml.feature.StopWordsRemover
+
+// COMMAND ----------
+
+val remover = new StopWordsRemover()
+    .setStopWords(StopWordsRemover.loadDefaultStopWords("english") 
+        ++ Array("!", "?", "@", "...", "'", ",", ".", "!","!!", "_","??","???", ":", ";", "-LRB-", "-RRB-", "#", "`","'s","-","&","''","``","n't","‰","¥","˜","ð","®","¹","¦","¶","•","-lsb-","-rsb-","amp"))
+    .setInputCol("lemmas")
+    .setOutputCol("filtered_lemmas")
+
+val LemmeswithoutStopwords = remover.transform(withLemmes)
+
+    
+
+// COMMAND ----------
+
+val LemmeswithoutStopwordsTest = remover.transform(withLemmesTest)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.2.1 Counted Lemme
+
+// COMMAND ----------
+
+/*display(LemmeswithoutStopwords
+    .select(explode(col("filtered_lemmas")).as("filtered_lemmas"))
+    .groupBy("filtered_lemmas")
+    .count()
+    .orderBy(col("count").desc).limit(10))*/
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.3 Lemmetization to Tokenization
+
+// COMMAND ----------
+
+
+val LemmeswithoutStopwordsString = LemmeswithoutStopwords.withColumn("filtered_lemmas", concat_ws(" ",col("filtered_lemmas"))) //pour passer de lemme  tokenization
+
+
+//val withTokens = LemmeswithoutStopwordsString.withColumn("tokens", tokenize(col("filtered_lemmas")))
+
+//withTokens.show(7)
+
+//test data
+
+val LemmeswithoutStopwordsStringTest = LemmeswithoutStopwordsTest.withColumn("filtered_lemmas", concat_ws(" ",col("filtered_lemmas"))) //pour passer de lemme  tokenization
+
+
+//val withTokensTest = LemmeswithoutStopwordsStringTest.withColumn("tokens", tokenize(col("filtered_lemmas")))
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.3.1 Counted Tokens
+
+// COMMAND ----------
+
+/*display(withTokens
+    .select(explode(col("tokens")).as("token"))
+    .groupBy("token")
+    .count()
+    .orderBy(col("count").desc)
+    .limit(15))*/
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.1 Tweet to tokenization
+
+// COMMAND ----------
+
+//val withTokensSW = tweetFeel.withColumn("tokens", tokenize(col("OriginalTweet")))
+
+// COMMAND ----------
+
+
+
+/*display(withTokensSW .select(explode(col("tokens")).as("token")).groupBy("token").count().orderBy(col("count").desc).limit(15))*/
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.2 Remove Stop words
+
+// COMMAND ----------
+
+/*import org.apache.spark.ml.feature.StopWordsRemover
+
+val remover = new StopWordsRemover()
+    .setStopWords(StopWordsRemover.loadDefaultStopWords("english") 
+        ++ Array("!", "?", "@", "...", "'", ",", ".", "!", "_", "?", ":", ";", "-LRB-", "-RRB-", "#", "`","'s","-","&","''","``","n't","get"))
+    .setInputCol("tokens")
+    .setOutputCol("filtered_tokens")
+
+val withoutStopwords = remover.transform(withTokensSW)*/
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.3 Counted Tokens
+
+// COMMAND ----------
+
+/*display(withoutStopwords
+    .select(explode(col("filtered_tokens")).as("token"))
+    .groupBy("token")
+    .count()
+    .orderBy(col("count").desc)
+    .limit(15))*/
+
+
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ###TF-IDF Calcul de la matrice TF-IDF
+
+// COMMAND ----------
+
+import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+val tokenizer = new Tokenizer().setInputCol("filtered_lemmas").setOutputCol("tokens")
+val regexTokenizer = new RegexTokenizer()
+  .setInputCol("filtered_lemmas")
+  .setOutputCol("tokens")
+  .setPattern("\\W")
+// alternatively .setPattern("\\w+").setGaps(false)
+
+//val countTokens = udf { (words: Seq[String]) => tokens.length }
+
+val withTokens = tokenizer.transform(LemmeswithoutStopwordsString)
+val withTokensTest = tokenizer.transform(LemmeswithoutStopwordsStringTest)
+
+// COMMAND ----------
+
+val hashingTF = new HashingTF()
+  .setInputCol("tokens").setOutputCol("rawFeatures").setNumFeatures(6000)
+
+// COMMAND ----------
+
+//wordsData.show(10)
+
+// COMMAND ----------
+
+val featurizedData = hashingTF.transform(withTokens)
+//featurizedData.show(10)
+
+val featurizedDataTest = hashingTF.transform(withTokensTest)
+
+// COMMAND ----------
+
+//val featurizedDataRDD = featurizedData.select("rawFeatures").rdd.map(_.getAs[org.apache.spark.ml.linalg.Vector](0)).map(org.apache.spark.mllib.linalg.Vectors.fromML)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### Count Vector
+
+// COMMAND ----------
+
+// alternatively, CountVectorizer can also be used to get term frequency vectors
+
+/*val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+val idfModel = idf.fit(featurizedData)
+
+val rescaledData = idfModel.transform(featurizedData)
+rescaledData.select("Location","feelings", "features").show()*/
+
+// COMMAND ----------
+
+import org.apache.spark._
+import org.apache.spark.rdd._
+
+//val tweet= ratingsDS.select("OriginalTweet")
+
+//val tweetRDD: org.apache.spark.rdd.RDD[org.apache.spark.sql.Row] = tweet.rdd
+/*val rescaledData2 = rescaledData.select("Feelings", "features")
+val rescaledDataRDD = rescaledData2.rdd.map(f => (f.getAs[String](0),f.getAs[String](1)))*/
+
+// COMMAND ----------
+
+
+featurizedData.select("Feelings","filtered_lemmas").printSchema()
+featurizedDataTest.select("Feelings","filtered_lemmas").printSchema()
+
+// COMMAND ----------
+
+val featurizedData2=featurizedData.select("Feelings","filtered_lemmas")
+val featurizedData2Test=featurizedDataTest.select("Feelings","filtered_lemmas")
+
+
+// COMMAND ----------
+
+//featurizedData2.show()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### Naive Bayes CV
+
+// COMMAND ----------
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.sql.Row
+
+// COMMAND ----------
+
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels in index.
+// Fit on whole dataset to include all labels in index.
+val labelIndexer = new StringIndexer()
+  .setInputCol("Feelings")
+  .setOutputCol("indexedLabel")
+ 
+// Automatically identify categorical features, and index them.
+// Set maxCategories so features with > 4 distinct values are treated as continuous.
+
+//val labelDf = labelIndexer.fit(featurizedData2)//.transform(featurizedData2)
+val labelDf = labelIndexer.fit(featurizedData2).transform(featurizedData2)
+val labeltestDf = labelIndexer.fit(featurizedData2Test).transform(featurizedData2Test)
+
+// COMMAND ----------
+
+labeltestDf.printSchema()
+labelDf.show(10)
+
+// COMMAND ----------
+
+  // training data set - 70%
+  // test data set - 30%
+  val seed = 5043
+  val Array(trainingData0, testData0) = labelDf.randomSplit(Array(0.9, 0.1), seed)
+  val Array(trainingData1, testData1) = labeltestDf.randomSplit(Array(0.8, 0.2), seed)
+
+
+
+// COMMAND ----------
+
+val Data=trainingData0.union(trainingData1)
+val test=testData0.union(testData1)
+ 
+   val Array(trainingData, testData) = Data.randomSplit(Array(0.8, 0.2), seed)
+val labeltestDf =test
+
+// COMMAND ----------
+
+trainingData.cache()
+testData.cache()
+labeltestDf.cache()
+
+// COMMAND ----------
+
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit, CrossValidator}  
+
+// Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and lr.
+val tokenizer = new Tokenizer()
+  .setInputCol("filtered_lemmas")
+  .setOutputCol("words")
+val hashingTF = new HashingTF()
+  .setInputCol(tokenizer.getOutputCol)
+  .setOutputCol("features")
+val naiveBayes = new NaiveBayes().setLabelCol("indexedLabel")
+  
+val pipeline = new Pipeline()
+  .setStages(Array(tokenizer, hashingTF, naiveBayes))
+
+
+
+
+
+// COMMAND ----------
+
+// We use a ParamGridBuilder to construct a grid of parameters to search over.
+// With 3 values for hashingTF.numFeatures and 2 values for lr.regParam,
+// this grid will have 3 x 2 = 6 parameter settings for CrossValidator to choose from.
+val paramGrid = new ParamGridBuilder()
+  .addGrid(hashingTF.numFeatures, Array(1000, 5000, 10000, 15000))
+  .addGrid(naiveBayes.smoothing, Array(0.1))
+  .build()
+val evaluator = new MulticlassClassificationEvaluator()
+  .setLabelCol("indexedLabel")
+  .setPredictionCol("prediction")
+  .setMetricName("accuracy")
+
+  val cv = new CrossValidator()
+    .setEstimator(pipeline)
+    .setEvaluator(evaluator)
+    .setEstimatorParamMaps(paramGrid)
+    .setNumFolds(2)
+
+
+
+// COMMAND ----------
+
+// You can then treat this object as the model and use fit on it.
+val CVmodel = cv.fit(trainingData)
+
+// COMMAND ----------
+
+
+// test model with validation data
+val cvPredictionDf = CVmodel.transform(testData)
+//cvPredictionTestDf.show(10)
+val cvAccuracy = evaluator.evaluate(cvPredictionDf)
+println(cvAccuracy)
+
+// COMMAND ----------
+
+// test model with test data
+val cvPredictionTestDf = CVmodel.transform(labeltestDf)
+//cvPredictionTestDf.show(10)
+val cvAccuracyTest = evaluator.evaluate(cvPredictionTestDf)
+println(cvAccuracyTest)
+
+// COMMAND ----------
+
+CVmodel.avgMetrics
+
+
+// COMMAND ----------
+
+CVmodel.getEstimatorParamMaps
+
+// COMMAND ----------
+
+CVmodel.params.toList.mkString("(", ",", ")")
+
+
+// COMMAND ----------
+
+CVmodel.explainParams()
+
+
+// COMMAND ----------
+
+CVmodel.bestModel.extractParamMap().toString()
+
+// COMMAND ----------
+
+import org.apache.spark.mllib.evaluation.MulticlassMetrics  
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics 
+val predictionAndLabels=cvPredictionDf.select("prediction","indexedLabel").rdd.map(f => (f.getAs[Double](0),f.getAs[Double](1)))
+
+// Instantiate a new metrics objects
+//val bMetrics = new BinaryClassificationMetrics(predictionAndLabels)
+val mMetrics = new MulticlassMetrics(predictionAndLabels)
+val labels = mMetrics.labels
+
+// Print out the Confusion matrix
+println("Confusion matrix:")
+println(mMetrics.confusionMatrix)
+
+
+// Precision by label
+labels.foreach { l =>
+  println(s"Precision($l) = " + mMetrics.precision(l))
+}
+
+// Recall by label
+labels.foreach { l =>
+  println(s"Recall($l) = " + mMetrics.recall(l))
+}
+
+// False positive rate by label
+labels.foreach { l =>
+  println(s"FPR($l) = " + mMetrics.falsePositiveRate(l))
+}
+
+// F-measure by label
+labels.foreach { l =>
+  println(s"F1-Score($l) = " + mMetrics.fMeasure(l))
+}
+
+// COMMAND ----------
+
+val predictionAndLabelsTest=cvPredictionDf.select("prediction","indexedLabel").rdd.map(f => (f.getAs[Double](0),f.getAs[Double](1)))
+
+// Instantiate a new metrics objects
+//val bMetrics = new BinaryClassificationMetrics(predictionAndLabels)
+val mMetrics = new MulticlassMetrics(predictionAndLabels)
+val labels = mMetrics.labels
+
+// Print out the Confusion matrix
+println("Confusion matrix:")
+println(mMetrics.confusionMatrix)
+
+
+// Precision by label
+labels.foreach { l =>
+  println(s"Precision($l) = " + mMetrics.precision(l))
+}
+
+// Recall by label
+labels.foreach { l =>
+  println(s"Recall($l) = " + mMetrics.recall(l))
+}
+
+// False positive rate by label
+labels.foreach { l =>
+  println(s"FPR($l) = " + mMetrics.falsePositiveRate(l))
+}
+
+// F-measure by label
+labels.foreach { l =>
+  println(s"F1-Score($l) = " + mMetrics.fMeasure(l))
+}
+
+// COMMAND ----------
+
+val bMetrics = new BinaryClassificationMetrics(predictionAndLabels)
+// Precision by threshold
+val precision = bMetrics.precisionByThreshold
+precision.foreach { case (t, p) =>
+  println(s"Threshold: $t, Precision: $p")
+}
+
+// Recall by threshold
+val recall = bMetrics.recallByThreshold
+recall.foreach { case (t, r) =>
+  println(s"Threshold: $t, Recall: $r")
+}
+
+// Precision-Recall Curve
+val PRC = bMetrics.pr
+
+// F-measure
+val f1Score = bMetrics.fMeasureByThreshold
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 1")
+}
+
+val beta = 0.5
+val fScore = bMetrics.fMeasureByThreshold(beta)
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+}
+
+// AUPRC
+val auPRC = bMetrics.areaUnderPR
+println("Area under precision-recall curve = " + auPRC)
+
+
+// Compute thresholds used in ROC and PR curves
+val thresholds = precision.map(_._1)
+
+// ROC Curve
+val roc = bMetrics.roc
+
+// AUROC
+val auROC = bMetrics.areaUnderROC
+println("Area under ROC = " + auROC)
+
+// COMMAND ----------
+
+val bMetrics = new BinaryClassificationMetrics(predictionAndLabelsTest)
+// Precision by threshold
+val precision = bMetrics.precisionByThreshold
+precision.foreach { case (t, p) =>
+  println(s"Threshold: $t, Precision: $p")
+}
+
+// Recall by threshold
+val recall = bMetrics.recallByThreshold
+recall.foreach { case (t, r) =>
+  println(s"Threshold: $t, Recall: $r")
+}
+
+// Precision-Recall Curve
+val PRC = bMetrics.pr
+
+// F-measure
+val f1Score = bMetrics.fMeasureByThreshold
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 1")
+}
+
+val beta = 0.5
+val fScore = bMetrics.fMeasureByThreshold(beta)
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+}
+
+// AUPRC
+val auPRC = bMetrics.areaUnderPR
+println("Area under precision-recall curve = " + auPRC)
+
+
+// Compute thresholds used in ROC and PR curves
+val thresholds = precision.map(_._1)
+
+// ROC Curve
+val roc = bMetrics.roc
+
+// AUROC
+val auROC = bMetrics.areaUnderROC
+println("Area under ROC = " + auROC)
